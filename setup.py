8999b4a3fedf71df39c586486744610e0daa9599
Mostafa Moradian <mostafamoradian0@gmail.com>
2017-04-23 15:38:22 +0430

    updated setup.py.

--- setup.py
+++ setup.py
@@ -8,6 +8,16 @@ setup(name="pygrest",
       author_email="mostafamoradian0@gmail.com",
       license="GPLv3",
       packages=["grest"],
+      long_description="""
+gREST
+=====
+
+gREST (Graph-based REST API Framework) is a RESTful API development
+framework on top of Python, Flask, Neo4j and Neomodel. Its primary
+purpose is to ease development of RESTful APIs with little effort and
+miminum amount of code.
+
+For more information, visit github page of `the project <https://github.com/mostafa/grest>`_.""",
       install_requires=[
           "flask",
           "flask_classful",
